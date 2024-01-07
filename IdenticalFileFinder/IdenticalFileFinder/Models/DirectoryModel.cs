6bd6a780fd718e0277d81001115b220dce322220
williamjshipman <shipman.william@gmail.com>
2024-01-07 22:21:49 +0200

    Add methods to populate a directory hierarchy.

--- IdenticalFileFinder/IdenticalFileFinder/Models/DirectoryModel.cs
+++ IdenticalFileFinder/IdenticalFileFinder/Models/DirectoryModel.cs
@@ -43,5 +43,68 @@ namespace IdenticalFileFinder.Models
         {
             InitThisClass(path, null);
         }
+
+        public void Populate()
+        {
+            string[] files = Directory.GetFiles(AbsolutePath);
+            foreach (string file in files)
+            {
+                Files.Add(new FileModel(file, this));
+            }
+
+            string[] directories = Directory.GetDirectories(AbsolutePath);
+            foreach (string directory in directories)
+            {
+                SubDirectories.Add(new DirectoryModel(directory, this));
+            }
+        }
+
+        public void PopulateRecursive()
+        {
+            Populate();
+
+            foreach (DirectoryModel directory in SubDirectories)
+            {
+                directory.PopulateRecursive();
+            }
+        }
+
+        public async Task PopulateAsync()
+        {
+            await Task.Run(() => Populate());
+        }
+
+        public async Task PopulateRecursiveAsync()
+        {
+            await Task.Run(() => PopulateRecursive());
+        }
+
+        public void CalculateHashes()
+        {
+            foreach (FileModel file in Files)
+            {
+                file.CalculateHash();
+            }
+        }
+
+        public async Task CalculateHashesAsync()
+        {
+            await Task.Run(() => CalculateHashes());
+        }
+
+        public void CalculateHashesRecursive()
+        {
+            CalculateHashes();
+
+            foreach (DirectoryModel directory in SubDirectories)
+            {
+                directory.CalculateHashesRecursive();
+            }
+        }
+
+        public async Task CalculateHashesRecursiveAsync()
+        {
+            await Task.Run(() => CalculateHashesRecursive());
+        }
     }
 }
